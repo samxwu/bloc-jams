e0c4e7a84830f40a430ab752cf4fcafffda39350
Sam Wu <sam.x.wu@gmail.com>
2016-09-21 17:57:48 -0400

    DOM-Scripting-Events-Assignment: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var pointUpdate = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function(points) {
- 
-      
-    // Loop through the 'point' NodeList to scale each element & change its opacity 
-        
-        for(var i = 0; i < points.length; i++) {
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+    forEach(points, pointUpdate);          
  };
+
 
 window.onload = function() {
     
