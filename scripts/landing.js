cd1b017258a1d102344038bffd7f28ed563ec2b0
Sam Wu <sam.x.wu@gmail.com>
2016-09-21 16:40:21 -0400

    DOM-Scripting-Events: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
  
-     var points = document.getElementsByClassName('point');
-    
+      
     // Loop through the 'point' NodeList to scale each element & change its opacity 
         
         for(var i = 0; i < points.length; i++) {
@@ -11,3 +12,20 @@ var animatePoints = function() {
          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
         }
  };
+
+window.onload = function() {
+    
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+      
+ window.addEventListener('scroll', function(event) {
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);   
+     }
+ });   
+}
